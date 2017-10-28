d88c9ed4ceedad82cc822d32ae6032f71589c457
Thuong Nguyen <nmthuong@users.noreply.github.com>
2017-10-29 02:47:19 +0700

    Upload file project Web, code by PHP-Nuke

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+if ( ! defined( 'NV_MAINFILE' ) )
+{
+    die( 'Stop!!!' );
+}
+
+$db_config['dbhost'] = "localhost";
+$db_config['dbport'] = "";
+$db_config['dbname'] = "nukeviet";
+$db_config['dbuname'] = "root";
+$db_config['dbpass'] = "";
+$db_config['prefix'] = "vndes";
+
+$global_config['sitekey'] = "dd60e93b07daf2259182ab4bd253c5bf";// Do not change sitekey!
+
+?>
